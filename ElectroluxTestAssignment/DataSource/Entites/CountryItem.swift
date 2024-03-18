0022ede37b6c9cccbe27dc1952da0fef889f6de6
olofHammar <olle_hammar@hotmail.com>
2024-03-18 15:00:44 +0100

    Added struct CountryItem.

---
+++ ElectroluxTestAssignment/DataSource/Entites/CountryItem.swift
@@ -0,0 +1,57 @@
+//
+//  CountryItem.swift
+//  ElectroluxTestAssignment
+//
+//  Created by Olof Hammar on 2024-03-18.
+//
+
+import Foundation
+import SwiftUI
+
+/// An `Entity` representing a country item with essential details.
+struct CountryItem: Identifiable, Codable {
+    /// A unique identifier for the country item.
+    var id = UUID().uuidString
+    
+    /// The name of the country.
+    let name: String
+    
+    /// A reference to the flag of the country.
+    let flagImageString: String
+    
+    /// The currency used in the country.
+    let currency: String
+    
+    /// The languages spoken in the country.
+    let languages: [String]
+    
+    /// Initializes a new `CountryItem` with specified details.
+    /// - Parameters:
+    ///   - name: The name of the country.
+    ///   - flagRef: A reference to the flag of the country.
+    ///   - currency: The currency used in the country.
+    ///   - languages: The languages spoken in the country.
+    init(
+        name: String,
+        flagRef: String,
+        currency: String,
+        languages: [String]
+    ) {
+        self.name = name
+        self.flagImageString = flagRef
+        self.currency = currency
+        self.languages = languages
+    }
+}
+
+private extension CountryItem {
+    
+    /// Enum defining the coding keys for encoding and decoding `CountryItem`.
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name = "country_name"
+        case flagImageString = "ic_flag_belgium"
+        case currency = "country_currency"
+        case languages = "country_language"
+    }
+}
