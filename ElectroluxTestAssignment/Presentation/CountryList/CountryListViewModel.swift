a03fd5fccc30e45361fe3ddf0a7e250b961263aa
olofHammar <olle_hammar@hotmail.com>
2024-03-18 16:49:35 +0100

    Created CountryListViewModel and function for fetching countries.

---
+++ ElectroluxTestAssignment/Presentation/CountryList/CountryListViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  CountryListViewModel.swift
+//  ElectroluxTestAssignment
+//
+//  Created by Olof Hammar on 2024-03-18.
+//
+
+import Combine
+import Foundation
+
+final class CountryListViewModel: ObservableObject {
+    
+    @Published private(set) var countryList: [CountryItem] = []
+    
+    private let dataSource: CountryDataSource
+    
+    private var countrySubscription: AnyCancellable?
+    
+    init(
+        countryDataSource: CountryDataSource
+    ) {
+        self.dataSource = countryDataSource
+    }
+    
+    func fetchCountries() {
+        countrySubscription = dataSource.fetchCountries()
+            .subscribe(on: RunLoop.main)
+            .sink(receiveCompletion: { completion in
+                if case let .failure(error) = completion {
+                    print("Error fetching countries: \(error)")
+                }
+            }, receiveValue: { [unowned self] countries in
+                self.countryList = countries
+            })
+    }
+}
