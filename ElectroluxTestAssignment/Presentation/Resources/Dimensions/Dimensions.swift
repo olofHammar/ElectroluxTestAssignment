b23aaf51c0e76028b2158c8b89facd7df4e1b53f
olofHammar <olle_hammar@hotmail.com>
2024-03-19 08:50:40 +0100

    Created CGFloat extension for storing default dimension values.

---
+++ ElectroluxTestAssignment/Presentation/Resources/Dimensions/Dimensions.swift
@@ -0,0 +1,13 @@
+//
+//  Dimensions.swift
+//  ElectroluxTestAssignment
+//
+//  Created by Olof Hammar on 2024-03-19.
+//
+
+import Foundation
+
+extension CGFloat {
+    static let defaultPadding: CGFloat = 16
+    static let defaultCardHeight: CGFloat = 240
+}
